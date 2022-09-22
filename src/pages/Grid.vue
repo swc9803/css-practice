649d509863e5eb5cbb346d456080cb81de94c466
swc9803 <swc9803@gmail.com>
2022-09-22 15:25:48 +0900

    init

---
+++ src/pages/Grid.vue
@@ -0,0 +1,9 @@
+<template>
+  <h1>grid</h1>
+</template>
+
+<script setup lang="ts">
+console.log("grid");
+</script>
+
+<style lang="scss" scoped></style>
---
+++ src/pages/Home.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <h1>css-practice</h1>
+    <nav>
+      <ul>
+        <!-- <li v-for="(menu, i) in menus" :key="menu">
+          <router-link :to="{ path: menus[i] }">
+            {{ menus[i] }}
+          </router-link>
+        </li> -->
+      </ul>
+    </nav>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style lang="scss" scoped>
+ul {
+  width: 100%;
+  display: flex;
+  flex-wrap: wrap;
+}
+li {
+  font-size: 2em;
+  margin: 2%;
+  list-style: none;
+}
+</style>
